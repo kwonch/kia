99ffb4de8c33a74a3989b46c0f02ddbfd9e01c69
kwonch <rnjsskfen153@naver.com>
2021-12-07 17:58:52 +0900

    2021-12-07
    min768px 반응형 슬라이드 푸터 뉴스 간격  완료 내려가면서 생기는 효과 손봐야함
     769이상 1199이하 해야함

---
+++ javascript/media_768px.js
@@ -0,0 +1,75 @@
+if (window.matchMedia("(min-width: 1500px)").matches) {
+    $('.btn1 ').click(function () {
+            $('.car li:last').prependTo('.car');
+            $('.car').css('margin-left',-1500 );
+            $('.car').stop().animate({ marginLeft: 0 },750)
+        });
+
+
+
+        $('.btn2 ').click(function () {
+            $('.car').stop().animate({ marginLeft: -1500 }, 750, function () {
+                $('.car li:first').appendTo('.car');
+                $('.car').css({ marginLeft: 0 });
+            });
+        });
+} else {
+    const w=window.innerWidth; 
+
+    $('.btn1 ').click(function () {
+        $('.car li:last').prependTo('.car');
+        $('.car').css({marginLeft:-w});
+        $('.car').stop().animate({marginLeft:0},750);
+    });
+
+    $('.btn2 ').click(function () {
+        $('.car').stop().animate({marginLeft:-w},750, function(){
+          $('.car li:first').appendTo('.car');
+          $('.car').css({marginLeft:0});
+        });
+    });
+}
+
+ 
+
+        $(function(){
+    $(window).scroll(function(){
+        var ws=$(this).scrollTop();
+       
+        if(ws>300){
+            $('#section1').css("transform","translateY(0px)").css("opacity","1");
+        }
+        
+        if(ws>1300){
+            $('#section2').css("transform","translateY(0px)").css("opacity","1");
+        }
+        
+        if(ws>2200){
+            $('#section3').css("transform","translateY(0px)").css("opacity","1");
+        }
+        
+        if(ws>3000){
+            $('#section4').css("transform","translateY(0px)").css("opacity","1");
+        }
+        
+        if(ws>3900){
+            $('#section5').css("transform","translateY(0px)").css("opacity","1");
+        }
+        
+      
+
+    });
+});
+
+    $(function(){
+        $('.ham').click(function(){
+            $('.navigation2').slideToggle();
+        });
+    });
+
+    $(window).resize(function () {
+
+     if (window.innerWidth > 769);
+        $('.navigation2,').hide();
+            
+    });
